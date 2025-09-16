ecbfe577049b1ad57d11eb93a8e6d1d4704d5e85
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2025-09-16 22:28:05 +1000

    Add ELT to Airflow

--- tests/fct_orders_date_valid.sql
+++
@@ -1,2 +0,0 @@
-select * from {{ref('fct_orders')}}
-where date(order_date) > CURRENT_DATE() OR date(order_date) < date('1990-01-01')
--- tests/fct_orders_discount.sql
+++
@@ -1,2 +0,0 @@
-select * from {{ref('fct_orders')}}
-where item_discount_amount > 0
